abe1c3bc2d5d30dcd9f7623e926c2a6470989391
maomao <18739912538@163.com>
2019-01-09 11:06:31 +0800

    www

--- application/index/controller/Profit.php
+++ application/index/controller/Profit.php
@@ -8,26 +8,19 @@ class Profit extends Base
         if(input('size')){
             $this->size = input('size');
         }
-        if(input('type')==0){
-            $this->map[] = ['wal_type','in',[4,5,6]; 
-        }elseif(input('type')==1){
-            $this->map[] = ['wal_type','=',8]; 
-        }elseif(input('type')==2){
-            $this->map[] = ['wal_type','=',7]; 
-        }elseif(input('type')==3){
-            $this->map[] = ['wal_type','in',[1,2,3]];
-        }
-
+        $type = input('type')?input('type'):0;
+        $this->map[] =$this->ttype($type);
         $list = model('ProWal')->chaxun($this->map, $this->order, $this->size);
+        $this->msg($list);
+    }
+
+    public function ming(){
         $arr = [
-            0 => $list,
-            1 => $this->user['us_account'],
-            2 => $this->user['us_vip_account'],
+            'us_account' => $this->user['us_account'],
+            'us_vip_account' => $this->user['us_vip_account'],
         ];
         $this->msg($arr);
-
     }
-   
 
     public function ti(){
         if(is_post()){
@@ -68,80 +61,25 @@ class Profit extends Base
         $list = model('ProTixian')->chaxun($this->map, $this->order, $this->size);
         $this->msg($list);
     }
+    public function ttype($type){
 
-
-
-    // 茶币转账
-    // public function trans(){
-    //     if(is_post()){
-    //         $d = input('post.');
-
-    //         $validate = validate('Profit');
-    //         $res = $validate->scene('trans')->check($d);
-    //         if (!$res) {
-    //             $this->e_msg($validate->getError());
-    //         }
-
-    //         if($this->user['us_account']==$d['tr_account']){
-    //             $this->e_msg('您不能转给自己');
-    //         }
-
-    //         if(mine_encrypt($d['us_safe_pwd']) != $this->user['us_safe_pwd']){
-    //             $this->e_msg('安全密码不正确');
-    //         }else{ 
-    //             unset($d['us_safe_pwd']);
-    //         }
-           
-    //         if($this->user['us_wal']<$d['tr_num']){
-    //             $this->e_msg('茶币不足');
-    //         }
-    //         $rel = model('ProTransfer')->tianjia($d,$this->user['id']);
-    //         $this->msg($rel);
-
-    //     }else{
-            
-    //         $this->map[] = ['us_id|us_to_id', '=', $this->user['id']];
-    //         $list = model('ProTransfer')->chaxun($this->map, $this->order, $this->size);
-    //         foreach ($list as $k => $v) {
-    //             if($v['us_id']==$this->user['id']){
-    //                 $list[$k]['tr_num'] = '-'.$v['tr_num'];
-    //             }
-    //             $list[$k]['us_account'] = $this->user['us_account'];
-    //         }
-    //         $this->msg($list);
-        
-    //     }
-    // }
-    //转换
-    // public function convert(){
-    //     if(is_post()){
-    //         $d = input('post.');
-    //         $validate = validate('Profit');
-    //         $res = $validate->scene('convert')->check($d);
-    //         if (!$res) {
-    //             $this->e_msg($validate->getError());
-    //         }
-            
-    //         if(mine_encrypt($d['us_safe_pwd']) != $this->user['us_safe_pwd']){
-    //             $this->e_msg('安全密码不正确');
-    //         }else{
-    //             unset($d['us_safe_pwd']);
-    //         }
-
-    //         if($this->user['us_msc']<$d['convert_num']){
-    //             $this->e_msg('您的奖励不足');
-    //         }
-          
-    //         $rel = model("ProConvert")->tianjia($this->user['id'],$d['convert_num']);
-    //         $this->s_msg('转换成功');
-    //     }else{
-    //         $this->map[] = ['us_id','=',$this->user['id']];
-    //         if(input('size')){
-    //             $this->size = input('size');
-    //         }
-    //         $list = model('ProConvert')->chaxun($this->map, $this->order, $this->size);
-    //         $this->msg($list);
-    //     }
-    // }
+        switch (true) {
+            case $type==0:
+                $tt = ['wal_type','in',[4,5,6]];
+                break;
+            case $type==1:
+                $tt = ['wal_type','=',8]; 
+                break;
+            case $type==2:
+                 $tt = ['wal_type','=',7]; 
+                break;
+            case $type==3:
+                $tt = ['wal_type','in',[1,2,3]];
+                break;
+            default:
+                break;
+        }
+        return $tt;
+    }
     
 }
